6e7b86942348a71916c0e14d4157080c0e3be665
Simran Preet Singh <simran5590@gmail.com>
2022-06-29 09:20:40 -0700

    Folder structure to handle the project better

---
+++ When it's happening/PresentationLayer/Screens/WhatsHappeningListScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  When it's happening
+//
+//  Created by Simran Preet Narang on 2022-06-29.
+//
+
+import SwiftUI
+
+struct WhatsHappeningListScreen: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        WhatsHappeningListScreen()
+    }
+}
